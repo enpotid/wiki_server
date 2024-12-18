25f1af6c7ff8775ebafe7b434ad39b2f927df901
Duswnsey <hazer24879@gmail.com>
2024-12-18 20:49:29 +0900

    i

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const defaultEnvContent = `
+DB_HOST=localhost
+DB_PORT=5432
+DB_USER=myuser
+DB_PASSWORD=mypassword
+DB_DBNAME=mydatabase
+PORT=24879
+`;
+
+// .env 파일 경로
+const envFilePath = path.join(__dirname, '.env');
+
+// .env 파일이 존재하지 않으면 기본값으로 생성
+if (!fs.existsSync(envFilePath)) {
+    console.log('.env 파일이 없습니다. 기본값을 생성합니다.');
+    fs.writeFileSync(envFilePath, defaultEnvContent, 'utf8');
+    console.log('.env 파일이 생성되었습니다.');
+    process.exit(1);
+} else {
+    console.log('.env 파일이 이미 존재합니다.');
+}
 const { ConnectDB, sql } = require('./ConnectDB');
 ConnectDB();
 const express = require('express');
@@ -10,7 +33,6 @@ app.get(`${process.env.REQ_DOCUMENT}:docname`, (req, res) => {
         if (err) { throw err; }
         res.send(resdb.rows[0])
     })
-
 })
 
 app.listen(process.env.PORT, () => {console.log(`App listening on: ${process.env.PORT}`)})
