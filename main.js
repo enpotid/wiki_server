3d246d309a2080402663f2a0964db986d9adc900
owo404 <xian101221@gmail.com>
2024-12-18 22:55:40 +0900

    update and make

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-const { ConnectDB, sql } = require('./ConnectDB');
+const { insertDoc, updateDoc, ConnectDB, sql } = require('./ConnectDB');
 ConnectDB();
 const express = require('express');
 const app = express();
 require("dotenv").config();
 
-app.get(`${process.env.REQ_DOCUMENT}:docname`, (req, res) => {
+app.get(`/document/:docname`, (req, res) => {
     let docname = req.params.docname;
     sql.query(`SELECT * FROM doc WHERE title=$1`, [docname], (err, resdb) => {
         if (err) { throw err; }
         res.send(resdb.rows[0])
     })
+})
+
+app.get(`/documentmake/:title/:body`, (req, res) => {
+    try {
+        if (insertDoc(req.params.title, req.params.body)) {
+            res.send('Document created successfully');
+        }else{
+            res.status(500).send('Error inserting document');
+        }
+    } catch (err) {
+        res.status(500).send('Error inserting document');
+    }
+})
+
+app.get(`/documentupdate/:title/:body`, (req, res) => {
+    try {
+        if (updateDoc(req.params.title, req.params.body)) {
+            res.send('Document updated successfully');
+        }else{
+            res.status(500).send('Error inserting document');
+        }
+    } catch (err) {
+        res.status(500).send('Error inserting document');
+    }
 })
 
 app.listen(process.env.PORT, () => {console.log(`App listening on: ${process.env.PORT}`)})
