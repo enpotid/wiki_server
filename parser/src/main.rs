88e881673a609b09f1221e663f5295e0e11d155d
Duswnsey <157892378+Duswnsey@users.noreply.github.com>
2025-01-02 18:05:02 +0900

    err + 주석

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -57,70 +57,107 @@ fn bold_txt (arg:String) -> String {
     return String::from("Lets Be blod Guys!");
 }
 fn parse (buf:&mut String, hookslist:Vec<ParserHook>) -> String {
+    //파서 후크가 파스할 코드를 버퍼에 저장. (파서 후크는 파스가 끝나고 이 버퍼를 업데이트함)
     let mut buff = String::from(buf.as_str());
+    //실시간으로 파스된 문자열이 여기에 저장된다.
     let mut temp = String::new();
-    
+
+    //훅의 개수만큼 순회할 예정
     for hook in hookslist.iter() {
+        //파스된 문자열이 넣어져 있던 변수를 클리어 (훅의 순회 후에는 buf로 temp가 복사됨. 기존에 있던 데이터가 남아있으면 데이터가 2배~~완전 럭키비키자나~~가 되기 때문에 클리어를 진행)
         temp.clear();
+        후크가 파스한 텍스트의 개수. 나중에 문자열 파스 후에 원본에 반영할 때 쓰임
         let mut num_of_args = 0;
+        //왜쓰지
         let mut num_of_temps = 0;
+        //후크의 파라미터들을 저장. 여기 있던 내용을 후크에게 파라미터로 넘김
         let mut args:Vec<String> = Vec::new();
+        //문법의 시작 문자열
         let mut start = "";
+        //문법의 끝 문자열
         let mut end = "";
+        //해당 후크의 문법을 분석하여 문법의 시작, 끝 문자열을 저장
         let parsed: Option<(&str, &str)> = hook.grammar.split_once("ARG");
+
         if let Some((startg, endg)) = parsed {
             start = startg;
             end = endg;
         } else {
             println!("No 'ARG' found in the string");
         }
+        //문법이랑 원 파스할 문자가 같으면 카운트가 올라가는 변수
         let mut i = 0;
+        //문법을 닫을 때
+        let mut closei = 0;
+        //만약 true면 순회하는 문자열을 args에 저장한다. (문법의 끝 재외)
         let mut in_grammar=false;
+        //문법 체크용 길이 2개
         let correct_conut_start = start.len();
         let correct_conut_end = end.len();
+
+        //후크의 순회 시작.
         for ch in buff.chars() {
+            //순회하는 문자가 문법의 첫 문자랑 일치하면
             if ch == start.chars().nth(i).unwrap_or('F') && !in_grammar{
+                //문법이 일치함으로 카운트 업
                 i+=1;
+                //문법이 충분히 카운트 되어 시작 문법의 길이. 즉, 후크의 문법이 열리면 args를 만들고 문법 안에 들어왔음을 선언
                 if i == correct_conut_start {
+                    //이걸 안하면 index 페닉에 걸린다.
                     args.push("".to_string());
                     in_grammar = true;
                 }
             } else if in_grammar {
-                if ch == end.chars().nth(correct_conut_start-i).unwrap_or('U') {
-                    i-=1;
-                    if (correct_conut_start > correct_conut_end && i == correct_conut_start - correct_conut_end) || (correct_conut_start <= correct_conut_end && i == 0) {
+                end의 i(처음 진입 시 0)번째 문자에 접근. 닫는 문법의 시작이라면
+                if ch == end.chars().nth(closei).unwrap_or('U') {
+                    closei를 감소, 즉, end문법의 chars index를 1 증가시킴으로서 다음 문자가 end의 다음 문자와 일치 하는지 확인한다.
+                    i+=1;
+                    //문법이 닫혔을 시,
+                    if (correct_conut_end == closei) {
                         in_grammar = false;
                         num_of_args += 1;
                         num_of_temps+= 1;
                         i=0;
+                        //임시 문자열
                         temp.push_str("ARGㅇㅅㅇ");
-                        temp.push_str(num_of_temps.to_string().as_str());
+                        //임시 문자열의 일련번호
+                         temp.push_str(num_of_args.to_string().as_str());
                     }
-                } else if ch != end.chars().nth(correct_conut_start-i).unwrap_or('C') && i < correct_conut_end && i > 0{
-                    for val in 0..3-i{
+                //문법이 일치하지 않고, i가 원값이 아닐 시, 즉, i가 전에 문법의 끝이 일치한 적이 있었을 시
+                } else if ch != end.chars().nth(closei).unwrap_or('C') && i != correct_conut_end{
+                    //문법 문자열은 args에 기록을 남기지 않기에 문법 문자열이 아니라고 판단(전에 이미 문법이 닫힌 걸 판단해서 핸들링 하는 코드가 있기 때문에, 여기서는 무조건 문법 문자열이 아닐 수 밖에 없다)
+                    for val in 0..closei{
+                        //문자열 복구. 어짜피 이전 내용은 end 문법이링 똑같으니까.
                         args[num_of_args].push(end.chars().nth(val).unwrap_or('K'));
                     }
+                    //아까 문자열 복구시 현재 순회하는 문자열은 안넣었기 때문.
                     args[num_of_args].push(ch);
-                    i=correct_conut_start;
                 } else {
+                    //~~정규 표현식 쓸껄...~~
+                    //만약 다 해당이 안된다. (끝 문법과 관련 없는 텍스트다.) 그러면 그냥 args에 푸쉬
                     args[num_of_args].push(ch);
                 }
-            } else if ch != start.chars().nth(i).unwrap_or('R') && i <= correct_conut_start && i >= 1 {
+            // end of in_grammar
+            // 시작 문법이랑 일치 하지 않으면 (전에는 일치 했는데)
+            } else if ch != start.chars().nth(i).unwrap_or('R') && i != 0 {
+                //문자열 복구
                 for val in 0..i{
                     temp.push(start.chars().nth(val).unwrap_or('U'));
                 }
                 temp.push(ch);
+            //해당 사항 없다
             } else {
                 temp.push(ch);
             }
         }
-        //end of parse. But not parserhook iter. Now. args:Vec<String> has the paramiter of fun.
-        //and temp has replaced text.
-        //lets print it.
+        //end of parse. But not parserhook iter. Now. args:Vec<String> has the paramiters of fun.
+        //and temp has replaced text. (ARGㅇㅅㅇ<일련번호> 그거
         
         let a = String::from(temp.clone());
         let ii = 1;
         for value in args {
+            //ARGㅇㅅㅇ 그거 리플레이스 하면 안되는데. ㅈ대따 ㅋㅋ...
+           
             let mut arg: String = String::from("ARGㅇㅅㅇ");
             let _ = arg.push_str(ii.to_string().as_str());
             let valuefinal:String = (hook.function)(value);
