f749838a3c789f9f42df415fd6f2b559cfc3c50d
Duswnsey <hazer24879@gmail.com>
2025-01-17 10:37:03 +0900

    autorun parser

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ const app = express();
 require("dotenv").config();
 const document = require("./routes/document");
 const raw = require("./routes/raw");
+const { spawn } = require('child_process')
+const parser = spawn('parser.exe');
+parser.stdout.on('data', (data) => {
+  console.log(`[parser] ${data}`);
+})
+parser.stderr.on('data', (data) => {
+  console.error(`[parser] ${data}`);
+});
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
