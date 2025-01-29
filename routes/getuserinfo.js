f089d65f66b115a00d2dc2b97edd55eeed7cf18e
Duswnsey <hazer24879@gmail.com>
2025-01-29 23:17:01 +0900

    c

--- routes/getuserinfo.js
+++ routes/getuserinfo.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const app = express.Router();
-const session = require("express-session")
-const { sql } = require("../ConnectDB")
-const CryptoJS = require("crypto-js")
-const SHA256 = require("crypto-js/sha256")
 app.use(express.json());
 app.get(`/`, async (req, res) => {
     if (req.session.info == undefined) {
-        console.log("not login")
         res.send(JSON.stringify({message:"not logined"}))
     } else {
         res.send({name:req.session.info.name, createdtime:req.session.info.createdtime})
