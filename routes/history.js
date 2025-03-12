dc83066b429bbfcc0b21953869b9ec4aa8dffedd
Duswnsey <hazer24879@gmail.com>
2025-03-13 02:28:36 +0900

    토론을 위한 준비 및, 리비전 숨기기 기능 추가.

--- routes/history.js
+++ routes/history.js
@@ -22,11 +22,11 @@ app.get(`/:namespace/:document/list/:nums/:pages`, (req, res) => {
     async function process(req, res, nums) {
         let namespace = req.params.namespace;
         let document = req.params.document;
-        let gethidden = ""
+        let gethidden = " AND hidden != true"
         if (req.session.info != undefined) {
-            gethidden = ((req.session.info.permission.includes("hide_rev") || req.session.info.permission.includes("owner")) ? ("") : (" AND hidden != false"))
+            gethidden = (req.session.info.permission.includes("hide_rev") || req.session.info.permission.includes("owner")) ? ("") : (" AND hidden != true")
         }
-        const resp = await sql.query(`SELECT hidden,rev,log,modifiedtime,author FROM history WHERE namespace=$1 AND title=$2${gethidden}`, [namespace, document])
+        const resp = await sql.query(`SELECT hidden,rev,log,modifiedtime,author FROM history WHERE namespace=$1 AND title=$2${gethidden} ORDER BY rev DESC`, [namespace, document])
         if (resp.rowCount == 0) {
             res.status(404).json({message:"not found"})
         } else {
@@ -46,19 +46,19 @@ app.get(`/:namespace/:document/:rev`, async (req, res) => {
     } else {
         if (resp.rows[0].hidden) {
             if (req.session.info != undefined) {
-                if (req.session.info.permission.includes("owner") || req.session.info.perms.includes("hide_rev") ) {
+                if (req.session.info.permission.includes("owner") || req.session.info.permission.includes("hide_rev") ) {
                     const response = await axios.post(
                         process.env.PARSER_SERVER,
                         JSON.parse(
                           `{"contents":${JSON.stringify(resp.rows[0].body).replace('"', '"')}}`
                         )
                       );
-                    res.json({message:"suc", body:response.data, log:resp.rows[0].log, hidden:resp.rows[0].log, modifiedtime:resp.rows[0].modifiedtime})
+                    res.json({message:"suc", body:response.data, log:resp.rows[0].log, hidden:resp.rows[0].hidden, modifiedtime:resp.rows[0].modifiedtime})
                 } else {
-                    res.json({message:"no perms"})
+                    res.json({message:"hidden"})
                 }
             } else {
-                res.json({message:"no perms"})
+                res.json({message:"hidden"})
             }
         } else {
             let candowiththisdic = await candowiththisdoc(for_acl.rows[0].acl, req)
@@ -73,6 +73,26 @@ app.get(`/:namespace/:document/:rev`, async (req, res) => {
             } else {
                 res.json({message:"no perms"})
             }
+        }
+    }
+})
+app.get(`/:namespace/:document/:rev/togglehide`, async (req, res) => {
+    let namespace = req.params.namespace;
+    let document = req.params.document;
+    let rev = req.params.rev
+    const resp = await sql.query(`SELECT * from history WHERE namespace=$1 AND title=$2 AND rev=$3`, [namespace, document, rev])
+    if (resp.rowCount == 0) {
+        res.status(404).json({message:"not found"})
+    } else {
+        if (req.session.info != undefined) {
+            if (req.session.info.permission.includes("owner") || req.session.info.permission.includes("hide_rev") ) {
+                sql.query(`UPDATE history SET hidden=$1 WHERE title=$2 AND namespace=$3 AND rev=$4`, [!resp.rows[0].hidden, document, namespace, rev])
+                res.json({message:"suc"})
+            } else {
+                res.json({message:"no perm"})
+            }
+        } else {
+            res.json({message:"no perm"})
         }
     }
 })
